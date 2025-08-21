2274f7d5f1f712ca14fb994ea7b6db3549b14de9
Hugo Siqueira <133659812+HugoBurch@users.noreply.github.com>
2025-08-21 18:24:21 -0300

    Add files via upload
    projeto inicial

---
+++ fila_de_banco/fila_de_banco/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using fila_de_banco;
+
+
+
+
+namespace test {
+      class Program {
+        fila Fila = new fila();
+
+        static void Main(string[] args) {
+            
+           
+            new Program().loopfila();
+           
+        }
+        
+
+        public  void loopfila()
+        {
+            Console.WriteLine("Fila de Atendimento do Banco                                                 |");
+            Console.WriteLine("-----------------------------------------------------------------------------|");
+            Console.WriteLine("escolha a opção:                                                             |");
+            Console.WriteLine("Gerar - Adicionar cliente na fila e o nome do cliente, separados por espaço: |");
+            Console.WriteLine("-----------------------------------------------------------------------------|");
+            Console.WriteLine("Chamar - Atender cliente da fila                                             |");
+            Console.WriteLine("-----------------------------------------------------------------------------|");
+            Console.WriteLine("Listar - Listar clientes na fila                                             |");
+            Console.WriteLine("-----------------------------------------------------------------------------|");
+            Console.WriteLine("Sair - Sair do programa                                                      |");
+            Console.WriteLine("-----------------------------------------------------------------------------|");
+
+
+            string escolha = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(escolha))
+            {
+                loopfila();
+                return;
+            }
+            else
+            {
+                string[] filaOpcao = escolha.Split(' ', 3);
+                string comando = filaOpcao[0];
+
+
+
+
+                switch (comando.ToLower())
+                {
+                    case "gerar":
+
+
+                        if (filaOpcao.Length >= 3)
+                        {
+                            
+                                string tipo = filaOpcao[1].ToLower();
+                                string nome = filaOpcao[2];
+                                if (tipo == "prioridade")
+                                {
+                                    Fila.AdicionarPrioridade(nome);
+
+                                    loopfila();
+                                    return;
+                                }
+                                else
+                                {
+                                    Fila.AdicionarNormal(nome);
+                                    loopfila();
+                                    return;
+
+                                } 
+                            
+                       
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nPor favor, forneça o tipo de cliente (normal ou prioridade) e o nome do cliente.\n");
+                            loopfila();
+
+                        }
+                        break;
+
+
+
+                    case "chamar":
+                        Fila.ChamarCliente();
+                        loopfila();
+                        
+
+                        break;
+                    case "listar":
+                        Console.WriteLine("Listando clientes na fila.");
+
+                        break;
+                    case "sair":
+                        Console.WriteLine("Saindo do programa.");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+            }
+
+        }
+
+    }
+    
+}
---
+++ fila_de_banco/fila_de_banco/fila.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fila_de_banco
+{
+    internal class fila
+    {
+
+
+        Queue<string> filaNormal = new Queue<string>();
+        Queue<string> filaPrioridade = new Queue<string>();
+
+
+        string[] vetorFila = ["2;2;1;2;1"];
+
+
+
+        public void AdicionarNormal(string nome)
+        {
+
+            filaNormal.Enqueue(nome);
+
+            Console.WriteLine($"Cliente {nome}  tipo: normal  adicionado à fila.");
+        }
+        public void AdicionarPrioridade(string nome)
+        {
+            filaPrioridade.Enqueue(nome);
+            Console.WriteLine($"Cliente prioritário {nome} adicionado à fila.");
+        }
+
+
+
+        public void ChamarCliente()
+        {
+            int i = 0;
+            if (vetorFila[i] == "1")
+            {
+
+
+
+                if (filaNormal.Count != 0)
+                {
+                    Console.WriteLine($"\nCliente fila normal {filaNormal.Dequeue()}");
+
+
+
+
+
+
+                }
+                else if (filaPrioridade.Count != 0)
+                {
+                    Console.WriteLine($"\nCliente fila prioritário {filaPrioridade.Dequeue()}");
+
+
+                }
+                else
+                {
+                    Console.WriteLine("\nNenhum cliente na fila.");
+
+                }
+                i++;
+
+
+
+
+            }
+            else
+            {
+                if (filaPrioridade.Count != 0)
+                {
+                    Console.WriteLine($"\nCliente fila prioritário {filaPrioridade.Dequeue()}");
+
+                }
+                else if (filaNormal.Count != 0)
+                {
+                    Console.WriteLine($"\nCliente fila normal {filaNormal.Dequeue()}");
+
+                }
+                else
+                {
+                    Console.WriteLine("\nNenhum cliente na fila.\n");
+
+
+
+                }
+                i++;
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
+
+        
+
+
+
+    
+
